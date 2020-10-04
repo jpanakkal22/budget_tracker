ad40c2bf8cf047415c8d29726c66bb6d3aaf86a8
New Dad <josh_panakkal3122@hotmail.com>
2020-10-03 20:22:55 -0400

    create assets and images folder, move icons into assets folder

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,8 +6,8 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/styles.css",
     "/manifest.webmanifest",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",    
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png",    
   ];
   
   const CACHE_NAME = "static-cache-v2";
